77dfdf04f74296f06c6cdf5c1a1738b5121c3317
Assel <assel.qabylqari@gmail.com>
2018-01-23 08:10:34 -0500

    Create maxOfThree.js

---
+++ maxOfThree.js
@@ -0,0 +1,48 @@
+
+/*
+
+Max Of Three
+
+Fullstack Academy - JavaScript JumpStart
+
+
+- Description
+
+Create the function `maxOfThree` that evaluates three numbers and returns the largest number out of the three.
+
+The function takes three number arguments and returns the highest number out of three.
+
+**Examples*
+
+- INPUT: `maxOfThree(5,4,10);`
+- OUTPUT: `10`
+
+- INPUT: `maxOfThree(7,7,4);`
+- OUTPUT: `7`
+
+Write Code Below
+*/
+//a: a>b>c, b: b>a>c: c: c>a>b
+
+function maxOfThree(a, b, c) {
+  if ((a >= b) && (a>=c)) {
+    return a;
+  }       //5  2      5 10
+  else if ((a>=b) && (a<=c)){
+    return c;
+  }
+  else {
+    return b;
+  }
+}
+
+/* another solution
+
+function maxOfThree(a,b,b){
+	if (a> b && a > c){
+		return a;
+	} else if(b > c){
+		return b;
+	} else {
+		return c;
+	} */
