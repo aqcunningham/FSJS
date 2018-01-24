daef60a0540f258b62d142a8585b549996fd0dfb
Assel <assel.qabylqari@gmail.com>
2018-01-23 23:00:38 -0500

    Create incrementByThree.js

---
+++ incrementByThree.js
@@ -0,0 +1,15 @@
+//**Create a For-Loop that increments by 3.**
+//Have your loop increment up to 15.  In your code block within your loop `console.log` your counter variable.  The final result should output:
+
+// 0
+// 3
+// 6
+// 9
+// 12
+// 15
+
+// START FOR-LOOP HERE
+
+for (ind = 0; ind<=15; ind+=3){
+  console.log(ind);
+}
