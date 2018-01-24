6d678459af7e731dc1f08ea3de535031f5ec4cc1
Assel <assel.qabylqari@gmail.com>
2018-01-23 22:45:33 -0500

    Create indChar.js

---
+++ indChar.js
@@ -0,0 +1,11 @@
+/*
+
+create a function logCharacters that console.logs induvidual characters in a string.
+
+*/function logg(str){
+  for (var ind=0; ind<str.length; ind+=1 ) {
+  console.log(str[ind]);  
+  }
+}
+
+logg('yes-no')
