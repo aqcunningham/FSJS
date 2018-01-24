58de0e1de484173e0a2db69f12f37a6219299ce0
Assel <assel.qabylqari@gmail.com>
2018-01-24 15:26:31 -0500

    Create logsOutChar.js

---
+++ logsOutChar.js
@@ -0,0 +1,8 @@
+/* create a function that console.logs induvidual characters in a string
+*/
+function n (str){
+  for (var index = 0; index<str.length; index++ ){
+  console.log(str[index]);    
+  }
+}
+n('lola');
