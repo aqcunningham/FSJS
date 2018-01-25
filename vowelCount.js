b6cf938c7b936fc86ca90a7ea8898f3ecf9392fc
Assel <assel.qabylqari@gmail.com>
2018-01-25 17:29:17 -0500

    Create vowelCount.js

---
+++ vowelCount.js
@@ -0,0 +1,39 @@
+/*
+#### Challenge - Vowel Count
+
+Create the function **vowelCount(str)** that takes a **str** parameter and returns the number of vowels the string contains (ie. "Fullstack Academy" would return 5).  
+
+Do not count 'y' as a vowel for this challenge.
+
+<br>
+
+####Examples
+
+```js
+- INPUT: vowelCount("Good Job");
+- OUTPUT: 3
+
+
+- INPUT: vowelCount('Yankees');
+- OUTPUT: 3
+```
+
+*/
+
+// Write Code Below:
+
+
+function vowelCount(str){
+  var fc = 0;
+  for (ind = 0; ind < str.length; ind++){
+    if (str[ind] === 'a'||str[ind] === 'e'||str[ind] === 'i'||str[ind] === 'o'||str[ind] === 'u'||str[ind] === 'A'||str[ind] === 'E'||str[ind] === 'I'||str[ind] === 'O'||str[ind] === 'U'){
+      fc+=1;
+    }
+  }
+  return fc;
+  //( v='a', 'e', 'i', 'o', 'u') in str 
+  // return cnt=v++
+}
+
+vowelCount('good jooaeb')
+
