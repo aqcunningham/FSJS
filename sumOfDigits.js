b51c5d721d78811b7924e4b3791d253b86c5fcdd
Assel <assel.qabylqari@gmail.com>
2018-01-25 19:25:12 -0500

    Create sumOfDigits.js

---
+++ sumOfDigits.js
@@ -0,0 +1,12 @@
+/*
+a decision, not full.
+function sumOfDigits(n){
+  
+  z = n.toString();
+  //for (n; n>=0; n--){
+    ff = (Number(z[0])+Number(z[1])+Number(z[2]));
+  
+  return ff;
+}
+
+sumOfDigits(568);*/
