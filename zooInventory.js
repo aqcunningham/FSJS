7d583bb7c90e12f83d24ee85a935a9cecb0e4e03
Assel <assel.qabylqari@gmail.com>
2018-02-10 21:09:07 -0500

    Create zooInventory.js

---
+++ zooInventory.js
@@ -0,0 +1,26 @@
+/**
+Write a function zooInventory that takes a zoo's inventory of animals (represented using nested arrays) and returns an array of strings that describe each animal's name, species, and age.
+
+Each input array will be nested 3 levels deep. Study the examples below to understand how the data is structured.
+
+Examples:
+
+var myZoo = [
+  ["King Kong", ["gorilla", 42]],
+  ["Nemo", ["fish", 5]],
+  ["Punxsutawney Phil", ["groundhog", 11]]
+];
+**/
+
+//somewhat correct solution
+function zooInventory(arr){
+  var newarr = '';
+    for (var i = 0; i<arr.length; i++){
+      //for (var j = 0; j<arr[i].length; j++){
+        //console.log(arr[i]+ 'the ' + arr[i][j] +'is');
+        newarr+=(arr[i][0] + ' the '+ arr[i][1][0] + ' is ' + arr[i][1][1]) + ', ';
+        //return;// + 'the ' +arr[j][0];
+      
+    }
+    return newarr;
+}
