a40d6b26d619874342ff097b8c21c7209c40423a
Assel <assel.qabylqari@gmail.com>
2018-02-14 15:38:46 -0500

    Update properNounFilter.js

--- properNounFilter.js
+++ properNounFilter.js
@@ -1,3 +1,20 @@
+/**Proper Noun Filter
+Write a function, properNounFilter, thate determines whether the string argument is a proper noun.
+
+A word is considered a proper noun if only the first letter is capitalized. If the argument is a proper noun, properNounFilter should return true. It should return false if the word isn't a proper noun, if the word is mixed case, or if it is all caps.
+
+Examples
+INPUT: properNounFilter('coffee');
+OUTPUT: false
+INPUT: properNounFilter('Einstein');
+OUTPUT: true
+INPUT: properNounFilter('ApoLLo');
+OUTPUT: false
+for(var i = 0; i<str.length; i++);
+  if(str.charCodeAt(0)>= 60 && str.charCodeAt(0)<=90){
+  
+**/
+
 //my solution, not entirly correct:
 function properNounFilter(str){
     for(var i = 0; i<str.length; i++){
@@ -12,3 +29,20 @@ function properNounFilter(str){
   //console.log(str.charCodeAt(j));
 }
 }
+//correct solution:
+
+function properNounFilter(str){
+  var code;
+    //for(var i = 0; i<str.length; i++){
+        if(str.charCodeAt(0)>97) return false; //why sometimes curly brakets are omitted?
+        
+            for(var j = 1;j<str.length;j++){
+              code = str.charCodeAt(j);
+              if(code<89) return false;
+    }
+      
+    
+  //console.log(str.charCodeAt(j));
+return true;
+
+}//                
