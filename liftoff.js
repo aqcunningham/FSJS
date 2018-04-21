0936af291703818cc197a9de84ebd0c46e10e19a
Assel <assel.qabylqari@gmail.com>
2018-04-21 00:28:11 -0400

    Create liftoff.js

---
+++ liftoff.js
@@ -0,0 +1,17 @@
+/*If your function were called with the number 5 being passed in to the function as follows: liftoff_countdown(5), the output would be:
+5
+4
+3
+2
+1
+Liftoff!*/
+
+function liftoff_countdown(n){
+while (n >=1){
+  		console.log(n);
+  		n -=1; //n--
+}
+console.log( 'liftoff!');
+  
+}
+
