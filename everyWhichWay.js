2ef42ab01c50aea41bb46e0c69f1a14a60de4bd8
Assel <assel.qabylqari@gmail.com>
2018-04-27 03:39:55 -0400

    Create everyWhichWay.js

---
+++ everyWhichWay.js
@@ -0,0 +1,29 @@
+// ### Every Which Way
+
+// Write a function `everyWhichWay` that accepts three number arguments. `everyWhichWay`
+// should:
+
+//   - return 'sum' if the sum of the first two numbers equals the third
+//   - return 'difference' if the first number minus the second equals the third
+//   - return 'product' if the product of the first two numbers equals the third
+//   - return 'fraction' if the first number divided by the second equals the third
+//   - return null if none of the above are true
+
+// ```javascript
+// everyWhichWay(10, 20, 30); // => sum
+
+// everyWhichWay(50, 20, 30); // => difference
+
+// everyWhichWay(4, 4, 16); // => product
+
+// everyWhichWay(100, 10, 10); // => fraction
+
+// everyWhichWay(1, 1000, 50); // => null
+
+var everyWhichWay=(a,b,c)=>{
+    return (a+b===c) ? 'sum'
+    : (a-b===c) ? 'difference'
+    : (a*b===c) ? 'product'
+    : (a/b===c) ? 'fraction'
+    : null
+}
