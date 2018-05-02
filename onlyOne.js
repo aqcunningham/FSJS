f682179ddc28af4f286f6504ec87abcfa6970061
Assel <assel.qabylqari@gmail.com>
2018-05-01 21:30:45 -0400

    Update onlyOne.js

--- onlyOne.js
+++ onlyOne.js
@@ -32,3 +32,8 @@ const onlyOne=(a,b,c)=> {
 const onlyOne=(a,b,c)=> {
     return (!!a && !b && !c) || (!a && !!b && !c) || (!a && !b && !!c);
 }
+
+//another cool solution:
+
+const onlyOne=(a,b,c)=> !!a+!!b+!!c === 1; //converts the true/false values into 1/0, and if only one is true, which is the task,
+//it will return true 
