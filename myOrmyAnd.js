e1c001ee538acfadfa5b94c2958e5151f49e882d
Assel <assel.qabylqari@gmail.com>
2018-05-01 20:25:25 -0400

    Create myOrmyAnd.js

---
+++ myOrmyAnd.js
@@ -0,0 +1,22 @@
+// // YOUR CODE BELOW
+// ### My Or, My And
+
+// Define a function, `myOr`, that accepts three arguments of any type.
+
+// `myOr` should return the same result as the buit-in `||` operator:
+
+// ```javascript
+// myOr(true, true, false) === (true || true || false); // => true
+// ```
+
+// Then, define a function, `myAnd`, that accepts three arguments of any type.
+
+// `myAnd` should return the same result as the buit-in `&&` operator:
+
+// ```javascript
+// myAnd(1, 'apples', true) === (1 && 'apples' && true); // => true
+// ```
+
+const myOr=(a,b,c)=> a || b || c //a little hazy
+
+const myAnd=(a,b,c)=> a && b && c 
