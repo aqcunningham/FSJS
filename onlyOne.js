bad8941c819afc0183a06b19dc1127a98598da29
Assel <assel.qabylqari@gmail.com>
2018-05-01 20:38:02 -0400

    Update onlyOne.js

--- onlyOne.js
+++ onlyOne.js
@@ -1,7 +1,34 @@
+// // YOUR CODE BELOW
+// ### Only One
+
+// Write a function `onlyOne` that accepts three arguments of any type.
+
+// `onlyOne` should return true only if exactly one of the three arguments are
+// truthy. Otherwise, it should return false.
+
+// Do not use the equality operators (`==` and `===`) in your solution.
+
+// ```javascript
+// onlyOne(false, false, true); // => true
+// onlyOne(0, 1, 2) // => false
+// ```
+
 function onlyOne(x, y, z) {
   return ((x && !y && !z) || (!x && y && !z) || (!x && !y && z));
 }
-
+//another:
 function onlyOne(x, y, z) {
   return (!!x + !!y + !!z === 1);
+}
+
+//another way:
+const onlyOne=(a,b,c)=> {
+    return (!!a && !b && !c) || (!a && !!b && !c) || (!a && !b && !!c)? true
+    : false 
+}
+
+//even shorter:
+
+const onlyOne=(a,b,c)=> {
+    return (!!a && !b && !c) || (!a && !!b && !c) || (!a && !b && !!c);
 }
