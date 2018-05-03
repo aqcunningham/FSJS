a63f8991bae4f5ed66655c0738c6bacd31da37d6
Assel <assel.qabylqari@gmail.com>
2018-05-03 13:50:48 -0400

    Create myUnshift.js

---
+++ myUnshift.js
@@ -0,0 +1,18 @@
+// // YOUR CODE BELOW
+// ### My Unshift
+
+// Write a function `unShift` that takes an array and a value of any type as arguments.
+
+// `unShift` should return a new array, with the given value as the first element
+// in the new array.
+
+// ```javascript
+// unShift([1, 2, 3], 0); / => [0, 1, 2, 3];
+// ```
+
+// Do not use the built-in .shift method for this problem!
+
+const myUnshift=(arr, v)=>{
+    return [v].concat(arr);
+    //return [v, ...arr];
+}
