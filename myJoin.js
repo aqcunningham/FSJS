f03aa093946da66dd1047d9a47942bc9b4946ac1
Assel <assel.qabylqari@gmail.com>
2018-05-15 01:47:58 -0400

    Update myJoin.js

--- myJoin.js
+++ myJoin.js
@@ -13,6 +13,36 @@ INPUT: myJoin([2, "be", false]);
 
 OUTPUT: '2,be,false'**/
 
+// ### My Join [bootcamp prep]
+
+// Define a function, `myJoin`, that accepts up to two arguments:
+//   1. array
+//   2. separator (string, optional)
+
+// `myJoin` should return a string with all of the elements from the array joined
+// togther. The separator should separate the joined elements:
+
+// ```javascript
+// myJoin(['let\'s', 'make', 'a', 'list'], ' '); // => "let's make  a list"
+// myJoin(['a', 'b', 'c'], '+'); // => "a+b+c"
+// ```
+
+// If separator is undefined, use ',' as the default separator.
+
+// ```javascript
+// myJoin(['Peter', 'Paul', 'Mary']); // => "Peter,Paul,Mary"
+// ```
+
+// If any elements in the array are undefined or null, they should be replaced with
+// an empty string in the returned string.
+
+// ```javascript
+// myJoin(['hello', undefined, 'world'], '-'); // => "hello--world"
+// ```
+
+// Do not use the built-in [`.join` array method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/joins) in your answer. Feel free to
+// use it in all future problems though!
+
 //my solution gives one exra comma at the end :|
 function myJoin(arr, sep){
   var newarr = '';
@@ -23,7 +53,6 @@ function myJoin(arr, sep){
       newarr+=arr[i]+',';
 //      newarr+= ' ';
     }
-    
   }
   return newarr;
 }
@@ -36,16 +65,28 @@ function myJoin(arr, sep){
       sep = ','; }
   for (var i = 0; i<arr.length; i++){
     if(arr[i]!== undefined && arr[i] !== null){
-      if (i === arr.length-1){
+      if (i === arr.length-1){ //what's this
         newarr+=arr[i];
       }
       //newarr+= sep + arr[i]; //why sep is first? dont we get ', arr[i], etc?
-     
     else{ 
       newarr+=arr[i]+sep;
     }
-    }
-    
+    } 
   }
   return newarr;
+}
+
+//another solution from prep:
+
+const myJoin=(arr,sep = ',')=>{
+  let final = '';
+  for(let i = 0; i<arr.length-1; i++){
+    if (arr[i] === undefined || arr[i] === null) final += sep;
+    else final += `${arr[i]}${sep}`;
+}
+if (arr.length){
+  final+=arr[arr.length-1];
+}
+return final;
 }
