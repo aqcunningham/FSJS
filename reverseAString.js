9e105d1fedaebdb45ceb73ea6c609771f3b808b1
Assel <assel.qabylqari@gmail.com>
2018-05-30 17:25:47 -0400

    Update reverseAString.js

--- reverseAString.js
+++ reverseAString.js
@@ -21,3 +21,26 @@ function reverseString(str){
   }
   return fs;
 }
+
+// // YOUR CODE BELOW### Backward String
+
+// Write a function, backwardString, that receives a string. It should log every
+// letter in the string, from the last character to the first. Use recursion.
+
+// NOTE: As with the last problem, the tests will check how many times console.log
+// is called.
+
+// ```javascript
+// backwardString('happy');
+// // y
+// // p
+// // p
+// // a
+// // h
+// ```
+
+const backwardString = (str)=>{
+    console.log(str[str.length-1]); //first, we print out the last letter;
+    if (str.length>1) backwardString(str.slice(0,-1)); //if we have a string (str.length>1), 
+    //then we call again the same function, but croppped from the back str.slice(0,-1) and do it all again
+}
