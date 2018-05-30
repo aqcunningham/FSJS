4a83f22da3ed0a58f47e6ae1dee7c98d3a1c1bda
Assel <assel.qabylqari@gmail.com>
2018-05-30 15:46:19 -0400

    Update incrementByThreeFL.js

--- incrementByThreeFL.js
+++ incrementByThreeFL.js
@@ -13,3 +13,23 @@
 for (ind = 0; ind<=15; ind+=3){
   console.log(ind);
 }
+
+//solution with a for loop:
+function countdown(n){
+  for ( i = 1; i<=n; i++){
+    console.log(i);
+  }
+  console.log ('liftoff!')
+}
+countdown(5)
+
+//solution with a recursion:
+function countdown (n){
+  if (n < 1){
+    console.log('countdown!');
+  }
+  else {
+    console.log(n);
+    countdown(n-1);
+  }
+}
